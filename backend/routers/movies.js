62356d18376463e91977620b660d06d0210b2a77
Darsh Wala <darshwala@gmail.com>
2026-07-22 13:29:17 +0530

    added basic deletion using id

--- backend/routers/movies.js
+++ backend/routers/movies.js
@@ -33,40 +33,57 @@ router.get("/:id", async (req, res) => {
 
 // POST add a movie to watchlist (fetches poster from OMDb if image not provided)
 router.post("/", async (req, res) => {
-
-    try {
-        const title = req.body.name;
-        if(!title){
-            res.status(400).json({ msg : "movie name is required" })
-        }
-        
-        const omdbRes = await fetch(`https://www.omdbapi.com/?apikey=${OMDB_API}&s=${encodeURIComponent(title)}`)
-
-        if(omdbRes.ok){
-            const omdbData = await omdbRes.json();
-
-            const usefulData = omdbData.Search[0]
-            // console.log(usefuxlData);   
-            res.json(usefulData);
-
-            const movie = {
-                name : usefulData.Title,
-                watched : false,
-                image : usefulData.Poster,
-            }
-            
-            console.log(movie);
-            
-            await Movie.create(movie);
-            
-        }
-        if(!omdbRes.ok){
-            console.log('Error Occured');            
-        }
-        
-    } catch (error) {
-        throw new Error(error);
+  try {
+    const title = req.body.name;
+    if (!title) {
+      res.status(400).json({ msg: "movie name is required" });
     }
+
+    const omdbRes = await fetch(
+      `https://www.omdbapi.com/?apikey=${OMDB_API}&s=${encodeURIComponent(title)}`,
+    );
+
+    if (omdbRes.ok) {
+      const omdbData = await omdbRes.json();
+
+      const usefulData = omdbData.Search[0];
+      // console.log(usefuxlData);
+      res.json(usefulData);
+
+      const movie = {
+        name: usefulData.Title,
+        watched: false,
+        image: usefulData.Poster,
+      };
+
+      console.log(movie);
+
+      await Movie.create(movie);
+    }
+    if (!omdbRes.ok) {
+      console.log("Error Occured");
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  const id = req.body.id;
+
+  try {
+    if (!id) {
+      res.status(400).json({ msg: "please provide id" });
+    } else {
+      const movieToBeDeleted = await Movie.findByIdAndDelete(id);
+      // await Movie.findByIdAndDelete(id);
+      res.json(movieToBeDeleted);
+    }
+    
+  } catch (error) {
+    console.log(`error occured ${error} `);
+    
+  }
 });
 
 export default router;
